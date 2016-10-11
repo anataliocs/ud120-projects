03979befc75f94f17db36334a88d52346ae7645f
Chris Anatalio <Chris.Anatalio@nike.com>
2016-10-11 13:30:32 -0700

    POI count from names text file

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,8 +19,20 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+#read text file and strip new lines
+poiNames = [line.rstrip('\n') for line in open("../final_project/poi_names.txt")]
 
-#print enron_data
+#names_file = pickle.load(open(, "r"))
+
+#print features
+#for d in enron_data["SKILLING JEFFREY K"]:
+#    print d
+
+#print number of features
+featureCount = 0
+for d in enron_data["SKILLING JEFFREY K"]:
+    featureCount += 1
+print "Feature Count:" , featureCount
 
 #size of data set
 count = 0
@@ -28,10 +40,17 @@ for d in enron_data:
     count += 1
 print "Record Count: " , count
 
-#How many POIs
+#How many POIs in Enron Data
 countPois = 0
 for d in enron_data:
     if enron_data[d]["poi"]:
         countPois += 1
 print "POI count: " , countPois
 
+#Total POIs
+
+countPoisFromNames = 0
+for l in poiNames:
+    countPoisFromNames += 1
+countPoisFromNames -= 2
+print "POIs from Names: " , countPoisFromNames
