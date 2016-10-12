13db9b951422de9397e783d74cc3e6e08f8f6734
Chris Anatalio <Chris.Anatalio@nike.com>
2016-10-12 13:36:09 -0700

    Total Payment is NaN

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -21,26 +21,51 @@
 """
 
 import pickle
-
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
-
 #read text file and strip new lines
 poiNames = [line.rstrip('\n') for line in open("../final_project/poi_names.txt")]
-
 #names_file = pickle.load(open(, "r"))
 
-#TOGGLE
-#ALL Data as JSON
-#------------------------
-#print enron_data
 
-#TOGGLE
-#All FEATURES
-#------------------------
-for d in enron_data["SKILLING JEFFREY K"]:
-    print d
 
-#Number of features
+#------------------------
+#TOGGLES
+#------------------------
+printAllData = False
+printAllFeatures = False
+printAllNames = False
+
+
+# TOGGLE
+# ALL Data as JSON
+#------------------------
+if printAllData:
+    print enron_data
+
+# TOGGLE
+# All FEATURES
+#------------------------
+if printAllFeatures:
+    for d in enron_data["SKILLING JEFFREY K"]:
+        print d
+
+# TOGGLE
+# Size of data set and/or Names
+# uncomment print d to print out NAMES
+# ------------------------
+if printAllNames:
+    for d in enron_data:
+        print d
+
+
+
+
+# ------------------------
+# DATA
+# ------------------------
+
+
+# Number of features
 #------------------------
 featureCount = 0
 for d in enron_data["SKILLING JEFFREY K"]:
@@ -48,18 +73,15 @@ for d in enron_data["SKILLING JEFFREY K"]:
 print "Feature Count:" , featureCount
 
 
-#TOGGLE
-#size of data set and/or Names
-#uncomment print d to print out NAMES
+# Size of data set and/or Names
 #------------------------
 count = 0
 for d in enron_data:
     count += 1
-    #print d
 print "Record Count: " , count
 
 
-#How many POIs in Enron Data
+# How many POIs in Enron Data
 #------------------------
 countPois = 0
 for d in enron_data:
@@ -68,7 +90,7 @@ for d in enron_data:
 print "POI count: " , countPois
 
 
-#Total POIs
+# Total POIs
 #------------------------
 countPoisFromNames = 0
 for l in poiNames:
@@ -77,28 +99,28 @@ countPoisFromNames -= 2
 print "POIs from Names: " , countPoisFromNames
 
 
-#Total value of the stock belonging to James Prentice?
+# Total value of the stock belonging to James Prentice?
 #------------------------
 print "James Prentice Total Stock Value: " , enron_data["PRENTICE JAMES"]["total_stock_value"]
 
-#How many email messages do we have from Wesley Colwell to persons of interest?
+# How many email messages do we have from Wesley Colwell to persons of interest?
 #------------------------
 print "Wesley Colwell Total Email Msgs: " , enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
 
-#What's the value of stock options exercised by Jeffrey K Skilling?
+# What's the value of stock options exercised by Jeffrey K Skilling?
 #------------------------
 print "Exercised Stock Options Jeffrey K Skilling: " , enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
 
 
-#Who took home the most money
+# Who took home the most money
 #------------------------
 print "Biggest take home"
-print "Lay: " , enron_data["LAY KENNETH L"]["total_payments"]
-print "Skilling: " , enron_data["SKILLING JEFFREY K"]["total_payments"]
-print "Fastow: " , enron_data["FASTOW ANDREW S"]["total_payments"]
+print "     Lay Takehome: " , enron_data["LAY KENNETH L"]["total_payments"]
+print "     Skilling Takehome: " , enron_data["SKILLING JEFFREY K"]["total_payments"]
+print "     Fastow Takehome: " , enron_data["FASTOW ANDREW S"]["total_payments"]
 
 
-#How many have a defined salary
+# How many have a defined salary
 #------------------------
 definedSalary = 0
 for d in enron_data:
@@ -106,10 +128,30 @@ for d in enron_data:
         definedSalary += 1
 print "Defined Salary count: " , definedSalary
 
-#How many have a known email
+# How many have a known email
 #------------------------
 knownEmail = 0
 for d in enron_data:
     if enron_data[d]["email_address"] != "NaN":
         knownEmail += 1
 print "Known Email count: " , knownEmail
+
+# How many people in the E+F dataset have "NaN" for their total payments?
+# What percentage of people in the dataset as a whole is this?
+nanTotalPayment = 0
+for d in enron_data:
+    if enron_data[d]["total_payments"] == "NaN":
+        nanTotalPayment += 1
+print "Unknown Total Payment count: " , nanTotalPayment
+print "% of Total Dataset" , ( float(nanTotalPayment) / float(count) ) * 100
+
+
+# How many POIs in the E+F dataset have NaN for their total payments?
+# What percentage of POI's as a whole is this?
+nanTotalPaymentPois = 0
+for d in enron_data:
+    if enron_data[d]["poi"]:
+        if enron_data[d]["total_payments"] == "NaN":
+            nanTotalPaymentPois += 1
+print "Unknown Total Payment of POIs count: " , nanTotalPaymentPois
+print "% of Total POIs" , ( float(nanTotalPaymentPois) / float(countPois) ) * 100
